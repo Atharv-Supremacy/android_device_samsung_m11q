57aca93eb155801679a91bd829db4c1efc9aef38
Atharv Negi <94751339+atharvnegi@users.noreply.github.com>
2022-12-12 20:54:00 +0530

    m11q: device.mk: fix typing error

--- device.mk
+++ device.mk
@@ -151,7 +151,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
-	$(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
