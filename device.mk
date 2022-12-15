12280dffb5a89a33dae84407f1988f0d8fd8bbe6
Atharv Negi <94751339+atharvnegi@users.noreply.github.com>
2022-12-15 19:03:14 +0530

    m11q: dont build overlay-lineage rn

--- device.mk
+++ device.mk
@@ -77,8 +77,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Offline Charger
 PRODUCT_PACKAGES += \
